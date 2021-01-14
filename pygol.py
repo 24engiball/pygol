b961f42f18e982b570e42cd2e6e027344d93f522
Mac Air <macair@Macs-MacBook-Air.local>
2021-01-15 02:20:01 +0700

    Lab1

--- pygol.py
+++ pygol.py
@@ -1,24 +1,18 @@
 from github import Github
 import os
-import ui
-#import curses
 import time
 from blessings import Terminal
 
 f = open('key.key','r')
 key = f.read()
-
 f.close()
 
-f = open('data.txt','r')
+f = open('md_data.txt','r')
 std_data = f.read()
 f.close()
 
 std_data = std_data.splitlines()
 stdlist =[]
-
-
-
 
 g = Github(key)
 
@@ -26,69 +20,40 @@ for std in std_data:
     std = std.split()
     stdlist.append(std)
 
-
-
-
-# coloring:
-# ui.info("This is", ui.red, "red",
-#         ui.reset, "and this is", ui.bold, "bold")
-
-# # enumerating:
-# list_of_things = ["foo", "bar", "baz"]
-# for i, thing in enumerate(list_of_things):
-#     ui.info_count(i, len(list_of_things), thing)
-
-# # progress indication:
-# ui.info_progress("Done",  5, 20)
-# ui.info_progress("Done", 10, 20)
-# ui.info_progress("Done", 20, 20)
-
-# # reading user input:
-# with_sugar = ui.ask_yes_no("With sugar?", default=False)
-
-# fruits = ["apple", "orange", "banana"]
-# selected_fruit = ui.ask_choice("Choose a fruit", fruits)
-
-# #  ... and more!
-
-
-# scr = curses.initscr()
-
  
-
-for std in stdlist:
-        repo = g.get_repo(std[4])
-        commit = repo.get_commit(repo.get_commits()[0].sha).raw_data['commit']['message']
-        std.append(commit)
-        
 term = Terminal()
-
-
-
+print(term.home + term.clear)
 for std in stdlist:
-            repo = g.get_repo(std[4])
+        try:
+            repo = g.get_repo(std[3])
             commit = repo.get_commit(repo.get_commits()[0].sha).raw_data['commit']['message']
-            std[5] = commit
+            std.append(repo)
+            std.append(commit)
+        except Exception as e:
+            print(std[3])
+            print(e)
+            exit()
+
 for i in range(len(stdlist)):
-
-    with term.location(0, i+2):
-                print(stdlist[i][0] + "                                            ")
-
+            with term.location(0, i+2):          
+                print(stdlist[i][1] + "     ")
 
 while True:
     try:
-    
         for i in range(len(stdlist)):
-            with term.location(10, i+2):
+            with term.location(20, i+2):
                 if(stdlist[i][5] == stdlist[0][5]):
-                    print(term.green + stdlist[i][5] + term.normal+ "                 ")
+                    print(term.green + stdlist[i][5][0:31] + "     " + term.normal)
                 else:
-                    print(term.red + stdlist[i][5] + term.normal+ "                 ")
-            time.sleep(1)
+                    print(term.red + stdlist[i][5][0:31]  + "     "+ term.normal)
 
         for std in stdlist:
-            repo = g.get_repo(std[4])
-            commit = repo.get_commit(repo.get_commits()[0].sha).raw_data['commit']['message']
+            
+            commit = std[4].get_commit(std[4].get_commits()[0].sha).raw_data['commit']['message']
             std[5] = commit
-    except:
+    except KeyboardInterrupt :
         exit()
+    except Exception as e:
+        print(e)
+        
+        exit() 
